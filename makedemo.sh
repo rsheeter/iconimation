3ef8fde9f7d0f5c096e6a6f878b5701ced456ee7
Rod S <rsheeter@google.com>
2024-01-17 17:32:43 -0800

    Initial hackery towards springs

--- makedemo.sh
+++ makedemo.sh
@@ -6,11 +6,12 @@ function generate() {
     local sample_file="$1"
     local animation="$2"
     local template="$3"
-    local font="$4"
+    local more_flags="$4"
+    local font="$5"
     # strip comments
     sed 's/#.*//g' "$sample_file" \
     | grep -v "^\S*$" \
-    | awk "{ print \"--codepoint 0x\"\$2\" --animation $animation --template resources/templates/"$template" --out-file demo/lottie/\"\$1\"-$animation-$template\" } " \
+    | awk "{ print \"--codepoint 0x\"\$2\" --animation $animation --template resources/templates/"$template" $more_flags --out-file demo/lottie/\"\$1\"-$animation-$template\" } " \
     | xargs -L1 target/release/iconimation-cli --font "$font" --debug
 }
 
@@ -24,9 +25,9 @@ cargo build --release
 #generate samples2.txt pulse-whole "$font"
 #generate samples2.txt pulse-parts "$font"
 
-generate samples.txt none ScalePosition.json "$font"
-generate samples.txt none ScaleRotate.json "$font"
-generate samples.txt none ScaleRotatePosition.json "$font"
+generate samples.txt none ScalePosition.json "" "$font"
+generate samples.txt none ScaleRotate.json "" "$font"
+generate samples.txt none ScaleRotatePosition.json "" "$font"
 
 python3 makedemo.py
 cp demo.html demo/
